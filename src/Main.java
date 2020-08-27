2ddfc154a5bb3d7017df2e5b9c77ec3ce2a8dd3f
AniruddhS24 <aniruddh.sriram@gmail.com>
2020-08-27 18:16:09 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import hangmangui.HangmanFrame;
+
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        //Scanner sc = new Scanner(new File(""));
+        //HangmanFrame f = new HangmanFrame();
+        HangmanFrame.mainmeth();
+    }
+}
